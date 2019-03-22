887263b299adc67fef4c305c8a710c97930fb4bf
Telmo Alexandre <telmo93@gmail.com>
2019-03-22 18:22:52 +0000

    Ssqueletodo Read
    Criado o componente Read.
    Alterado o index para adicionar o Read.
    
    Co-Authored-By: Tiago Costa <beardedcosta@users.noreply.github.com>

---
+++ Client/src/Components/FichaTecnica/Read.jsx
@@ -0,0 +1,113 @@
+import React, { Component } from "react";
+import AlertMsg from "../AlertMsg";
+
+class Create extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      alertText: "",
+      alertisNotVisible: true,
+      alertColor: "",
+      showRegFotografico: false
+    };
+  }
+
+  handleSubmit = async e => {
+    e.preventDefault();
+
+    //Objeto data
+    const data = {
+      // CAMPOS
+    };
+
+    //Enviar pedidos
+    const response = await fetch("/fichaTecnica/create", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    });
+    //Aguardar API
+    await response.json().then(resp => {
+      let status = resp.status;
+      switch (status) {
+        case "Success":
+          this.setState({
+            alertText: "Criado com sucesso",
+            alertisNotVisible: false,
+            alertColor: "primary"
+          });
+          break;
+        // case "INSUCESS":
+        //   this.setState({
+        //     alertText: "Utilizador ou palavra-passe erradas",
+        //     alertisNotVisible: false,
+        //     alertColor: 'danger'
+        //   });
+        //   break;
+        default:
+          console.log("A API ESTÁ A ARDER, DARIOOOOOOOOOOOOOOOOOOOOOO");
+      }
+    });
+  };
+
+  hideOrShow(){
+    this.setState({
+      showRegFotografico: !this.state.showRegFotografico
+    })
+  };
+
+  render() {
+    return (
+        <div className="Inicio container">
+            <p className="h4">Ficha Técnica</p>
+                <div className="form-group row">
+                    <label className="col-form-label col-md-2">Descrição do objeto:</label>
+                    <label className="col-form-label col-md-10">Descrição</label>
+                </div>
+                <div className="form-group row">
+                    <label className="col-form-label col-md-2">Processo LCRM N.º:</label>
+                    <label className="col-form-label col-md-1">255</label>
+                    <label className="col-form-label col-md">Data de entrada:</label>
+                    <label className="col-form-label col">20/01/2018</label>
+                    <label className="col-form-label col-md-3">Data de abertura de processo:</label>
+                    <label className="col-form-label col-md-2">25/01/2018</label>
+                </div>
+                <div className="form-group row">
+                    <label className="col-form-label col-md-2">Processo CEARC N.º:</label>
+                    <label type="number" id="nprocCEARC" className="form-control col-md-1" placeholder="N.º"/>
+                    <label className="col-form-label col-md">Data de entrada:</label>
+                    <input type="date" id="dataprocCEARC" className="form-control col"/>
+                    <label className="col-form-label col-md-3">Data de abertura de processo:</label>
+                  <input type="date" id="dataprocCEARC" className="form-control col-md-2"/>
+                </div>
+                <div className="form-group row">
+                  <label className="col-form-label col-md-4">Coordenação/Direção Técnica da Intervenção:</label>
+                  <input type="text" id="coordenacao" className="form-control col-md-8" placeholder="Coordenador/Diretor técnico"/>
+                </div>
+                <div className="form-group">
+                  <label className="h5 ">Registo fotográfico identificativo do objeto</label>
+                  <a style={{cursor:"pointer"}} onClick={()=>this.hideOrShow()}>
+                    <i className="fas fa-3x fa-plus-square m-3"/>
+                  </a>
+                  {
+                    this.state.showRegFotografico ?
+
+                    <div className="form-group row col-md-6">
+                      <label className="col-form-label col-md-3">Fotografia:</label>
+                      <input type="file" id="foto" className="col-form form-control-file col-md-9" accept="image/*" onChange=""/>
+                    </div>
+
+                    :null
+                  }
+                  
+                </div>
+                <AlertMsg text={this.state.alertText} isNotVisible={this.state.alertisNotVisible} alertColor={this.state.alertColor}/>
+                <button type="submit" className="btn btn-primary">Criar</button>
+        </div>
+    );
+  }
+}
+
+export default Create;
---
+++ Client/src/MasterComponents/ReadFichaTecnica.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import Header from "../Components/Header";
+import Footer from "../Components/Footer";
+import Read from "../Components/FichaTecnica/Read";
+
+class CreateFichaTecnica extends Component {
+  render() {
+    return (
+      <div className="ReadFichaTecnica">
+        <Header />
+        <Read />
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default CreateFichaTecnica;

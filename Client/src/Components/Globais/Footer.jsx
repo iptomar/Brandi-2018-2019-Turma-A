0dbdf932cde0eab95d35d0c67e6792d9caa4cf95
Tiago Areias <aluno19998@ipt.pt>
2019-04-09 14:03:19 +0100

    Adicionado Css ao Header e ao  Footer

--- Client/src/Components/Globais/Footer.jsx
+++ Client/src/Components/Globais/Footer.jsx
@@ -1,19 +1,61 @@
 import React, { Component } from "react";
+import '../../CssComponents/footer.css';
 
 class Footer extends Component {
-  render() {
-    return (
-      <div className="Footer">
-        <footer>
-          <div className="text-center py-3">2019, <a href="http://portal2.ipt.pt/" target="_blank" rel="noopener noreferrer">
-          Instituto Politécnico de Tomar
-            </a>{" "}
-            | Projeto de Sistemas de Informação - Licenciatura em Engenharia Informática
-          </div>
-        </footer>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<footer className="footer-distributed">
+
+				<div className="footer-left">
+
+					<p className="footer-company-name">Instituto Politécnico de Tomar</p>
+
+
+					<p className="footer-company-name">Laboratório de Conservação e Restauro</p>
+
+				</div>
+
+				<div className="footer-center">
+
+					<div>
+						<i className="fa fa-map-marker"></i>
+						<p><span>Quinta do Contador, Estrada da Serra</span>Tomar, Portugal</p>
+					</div>
+
+					<div>
+						<i className="fa fa-phone"></i>
+						<p>+351 249 328 100</p>
+					</div>
+
+					<div>
+						<i className="fa fa-envelope"></i>
+						<p><a href="mailto:helpdesk@ipt.pt">helpdesk@ipt.pt</a></p>
+					</div>
+
+				</div>
+
+				<div className="footer-right">
+
+					<p className="footer-company-about">
+						<span>Redes Sociais</span>
+
+					</p>
+
+					<div className="footer-icons">
+
+						<a href="https://www.facebook.com/iptomar"><i className="fa fa-facebook"></i></a>
+
+					</div>
+
+				</div>
+
+
+
+				<p className="footer" align="center">Brandi &copy; 2019</p>
+			</footer>
+
+		);
+	}
 }
 
 export default Footer;

064e3f353f9a1da0844561fcaa98e6ff7b7232eb
João Luís Ferreira <aluno19164@ipt.pt>
2019-04-10 15:54:59 +0100

    css

--- Client/src/index.js
+++ Client/src/index.js
@@ -30,19 +30,20 @@ ReactDOM.render(
             <Route path="/sobre" component={AboutPage} />
             <Route path="/contactos" component={ContactPage} />
             <Route path="/perfil" component={ProfilePage} />
-            {/* UTILIZADORES */}
+            {/* Utilizadores */}
             <Route path="/utilizadores/registar" component={RegisterPage} />
             <Route path="/utilizadores/:id/editar" component={(r) => <EditUserPage id={r.match.params.id} />} />
             <Route path="/utilizadores/:id/detalhes" component={(r) => <DetailsUserPage id={r.match.params.id} />} />
             <Route path="/utilizadores/listar:query?" component={(r) => <IndexPage query={r.match.params.query} />} />
-            {/* FICHA TÉCNICA */}
-            <Route path="/fichaTecnica/details/:id" component={DetailsFichaTecnica} />
+            {/* Ficha Técnica */}
+            <Route path="/fichaTecnica/:id/details" component={DetailsFichaTecnica} />
             <Route path="/fichaTecnica/criar" component={CreateFichaTecnica} />
             <Route path="/fichaTecnica" component={IndexFichaTecnica} />
             {/* Ficha Registo Identificacao */}
             <Route path="/fichaRI/criar" component={CreateFichaRI} />
             <Route path="/fichaRI/:id/detalhes:query?" component={(r) => <DetailsFichaRI id={r.match.params.id} query={r.match.params.query} />} />
             <Route path="/fichaRI/:query?" component={(r) => <IndexFichaRI query={r.match.params.query} />} />
+            {/* Default */}
             <Route component={LoginPage} />
         </Switch>
     </ BrowserRouter>

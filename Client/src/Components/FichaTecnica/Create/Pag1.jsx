ea0cd92b921b5e4f72179be6852337a258a4df19
Ricardo Pinheiro <47854257+aluno20182@users.noreply.github.com>
2019-04-16 23:42:12 +0100

    Criação da pagina 1 - Ficha Tecnica

--- Client/src/Components/FichaTecnica/Create/Pag1.jsx
+++ Client/src/Components/FichaTecnica/Create/Pag1.jsx
@@ -1,10 +1,15 @@
 import React, { Component } from "react";
 
 class Pag1 extends Component {
+  constructor(props){
+    super(props);
+
+  }
 
   render() {
     return (
       <div className="Inicio container">
+        <h1>Ficha Tecnica</h1>
       </div>
     );
   }

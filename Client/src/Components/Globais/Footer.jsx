dd6f064f302f7c2aa59dec8433b27547f814a818
João Nunes <nunesbusiness@gmail.com>
2019-04-30 21:04:01 +0100

    new footer (missing parts)
    ready for improvement.
    
    The new design is almost ready but need some adjustments.
    missing:
     - icons positions
     - font needs to be resized
    -  footer needs to be smaller

--- Client/src/Components/Globais/Footer.jsx
+++ Client/src/Components/Globais/Footer.jsx
@@ -4,55 +4,88 @@ import '../../CssComponents/footer.css';
 class Footer extends Component {
 	render() {
 		return (
-			<footer className="footer-distributed">
+			<footer>
+       
+       <div class="container-footer-all">
+        
+            <div class="container-body">
 
-				<div className="footer-left">
+                <div class="colum1">
+                    <h1>Informação da aplicação</h1>
 
-					<p className="footer-company-name">Instituto Politécnico de Tomar</p>
+                    <p>Esta aplicação foi criada para o Laboratório de Conservação e Restauro
+						do Instuto Politecnico de Tomar</p>
 
+                </div>
 
-					<p className="footer-company-name">Laboratório de Conservação e Restauro</p>
+                <div class="colum2">
 
-				</div>
+                    <h1>Redes Sociais</h1>
+				<div class="column2_n">
+                    <div class="row">
+					<a href="https://www.facebook.com/iptomar" class="btn btn-block btn-social btn-twitter btn_edit">
+    					<span class="fa fa-facebook">Segue o IPT no Facebook</span> 
+  					</a>
+                    </div>
 
-				<div className="footer-center">
-
-					<div>
-						<i className="fa fa-map-marker"></i>
-						<p><span>Quinta do Contador, Estrada da Serra</span>Tomar, Portugal</p>
-					</div>
-
-					<div>
-						<i className="fa fa-phone"></i>
-						<p>+351 249 328 100</p>
-					</div>
-
-					<div>
-						<i className="fa fa-envelope"></i>
-						<p><a href="mailto:helpdesk@ipt.pt">helpdesk@ipt.pt</a></p>
-					</div>
-
-				</div>
-
-				<div className="footer-right">
-
-					<p className="footer-company-about">
-						<span>Redes Sociais</span>
-
-					</p>
-
-					<div className="footer-icons">
-
-						<a href="https://www.facebook.com/iptomar"><i className="fa fa-facebook"></i></a>
-
-					</div>
-
+                    <div class="row">
+					<a href="https://www.instagram.com/ipt.politecnicodetomar/" class="btn btn-block btn-social btn-twitter btn_edit">
+    					<span class="fa fa-instagram"> Segue o IPT no Instagram</span>
+  					</a>
+                    </div>
+                    <div class="row">
+                    
+						<a href="https://twitter.com/iptomar" class="btn btn-block btn-social btn-twitter btn_edit">
+    					<span class="fa fa-twitter">Segue o IPT no Twitter </span> 
+  						</a>
+                    </div>
+					
 				</div>
 
 
+                </div>
 
-				<p className="footer" align="center">Brandi &copy; 2019</p>
-			</footer>
+                <div class="colum3">
+
+                    <h1>Informações de Contacto</h1>
+					<div class="column2_n">
+						<div class="row2">
+							<span class="fa fa-home btn_edit" aria-hidden="true"><label>Quinta do Contador . 
+							Estrada da Serra . 2300-313 . 
+							Tomar . Portugal</label></span>
+                   		</div>
+
+                   		<div class="row2">
+                       
+                        	<span class="fa fa-phone btn_edit " aria-hidden="true" ><label>+1-829-395-2064</label></span>
+                    	</div>
+
+                    	<div class="row2">
+							<span class="fa fa-envelope btn_edit" aria-hidden="true"> <label>geral@ipt.pt</label></span> 
+                    	</div>
+					
+				
+					</div>
+
+                  
+                </div>
+
+            </div>
+        
+        </div>
+        
+        {/*<div class="container-footer">
+               <div class="footer">
+                    <div class="copyright">
+                        © 2019 | <a href="">Brandi</a>
+                    </div>
+
+
+                </div>
+
+            </div>
+        */}
+</footer>
 
 		);
 	}

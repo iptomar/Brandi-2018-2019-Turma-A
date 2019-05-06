1aada2bb3b123d48b5e8487c48a359300c6d2ec9
lilozzy <pc465690@gmail.com>
2019-05-06 16:21:18 +0100

    Teste Criar Obra
    - Upload de 2 Fotografias para testar o botao de carregar fotografia.
    
    -criaçao do teste de Criar obra
    
    Imcompleto devido a nao ser possivel criar a obra

--- TestesClient/testesBrandi/src/test/java/TestarCriarObra.java
+++ TestesClient/testesBrandi/src/test/java/TestarCriarObra.java
@@ -45,7 +45,7 @@ public class TestarCriarObra {
 
     @Before
     public void setUp() {
-        driver.get("brandi.ipt.pt:81/fichaRI");
+        
     }
 
     @After
@@ -56,9 +56,43 @@ public class TestarCriarObra {
     // The methods must be annotated with annotation @Test. For example:
     //
     @Test
-    public void testarCriarObra() {
-    
-    
+    public void testarCriarObra() throws InterruptedException {
+        driver.get("brandi.ipt.pt:81");
+        WebElement btnAddFicha = driver.findElement(By.className("btn-success"));
+        btnAddFicha.click();
+        
+        WebElement dObjeto = driver.findElement(By.id("dObjeto"));
+        dObjeto.sendKeys("Reliquia");
+        WebElement procLCRM = driver.findElement(By.id("procLCRM"));
+        procLCRM.sendKeys("something");
+        WebElement procCEARC = driver.findElement(By.id("procCEARC"));
+        procCEARC.sendKeys("something");
+        WebElement dateEntrada = driver.findElement(By.id("dateEntrada"));
+        dateEntrada.sendKeys("06/05/2019");
+        WebElement dateConclusao = driver.findElement(By.id("dateConclusão"));
+        dateConclusao.sendKeys("07/05/2019");
+        WebElement dateEntrega = driver.findElement(By.id("dateEntrega"));
+        dateEntrega.sendKeys("08/05/2019");
+        WebElement coord = driver.findElement(By.id("coord"));
+        coord.sendKeys("someone");
+        WebElement dirTecn = driver.findElement(By.id("dirTecn"));
+        dirTecn.sendKeys("someone");
+        WebElement tecnicosCheckbox = driver.findElement(By.id("tecnicosCheckbox"));
+        tecnicosCheckbox.click();
+        WebElement propDonObra = driver.findElement(By.id("propDonObra"));
+        propDonObra.sendKeys("someone");
+        WebElement contact = driver.findElement(By.id("contact"));
+        contact.sendKeys("912345678");
+        WebElement endPostLocal = driver.findElement(By.id("endPostLocal"));
+        endPostLocal.sendKeys("IPT");
+        
+        WebElement chooseFoto = driver.findElement(By.className("custom-file-input"));
+        chooseFoto.click();
+//        chooseFoto.sendKeys("C:\\Brandi-2018-2019-Turma-A\\TestesClient\\testesBrandi\\img\\img2.jepg");
+        
+        WebElement btnCriar = driver.findElement(By.className("btn-success"));
+        btnCriar.click();
+        
     }
     
     

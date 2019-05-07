ac585e6e6c7f3d19bf43a351c2f7ee77828f5026
Tiago Areias <aluno19998@ipt.pt>
2019-05-07 17:41:30 +0100

    Página 4 da ficha técnica- continuação

--- Client/src/Components/FichaTecnica/Create/Pag4.jsx
+++ Client/src/Components/FichaTecnica/Create/Pag4.jsx
@@ -11,8 +11,30 @@ class Pag4 extends Component {
             alertColor: '',
         };
     }
-    //função que adiciona nova linha para a tabela 
-    adicionaLinha() {
+    adicionaNovaLinha = () => {
+        var table = document.getElementById("tabela");
+        //inserir nova linha
+        var row = table.insertRow(-1);
+        //parametros textArea
+        for (var i = 0; i < 4; i++) {
+            var novaCelula = row.insertCell(i);
+            var param = document.createElement('textarea');
+            novaCelula.appendChild(param);
+        }
+        //parametro input
+        //técnico
+        var cellTecnico = row.insertCell(4);
+        var paramTecnico = document.createElement('input');
+        paramTecnico.setAttribute("type","text")
+        paramTecnico.readOnly=true;
+        paramTecnico.value="Name";
+        cellTecnico.appendChild(paramTecnico);
+        //parametro date
+        //data
+        var cellDate = row.insertCell(5);
+        var paramData = document.createElement('input');
+        paramData.setAttribute("type","date")
+        cellDate.appendChild(paramData);
 
     }
 
@@ -81,9 +103,8 @@ class Pag4 extends Component {
                     </div>
                     <br />
                     <div className="row">
-                        <div className="input-group mr-3 col-md-3">
 
-                            <table className="table table-bordered table-secondary">
+                            <table className="table table-bordered table-secondary" id="tabela">
                                 <thead >
                                     <tr>
                                         <th scope="col">Tipo-Referência</th>
@@ -96,17 +117,21 @@ class Pag4 extends Component {
                                 </thead>
                                 <tbody>
                                     <tr>
+                                        <td id="param1"><textarea type="text"></textarea></td>
                                         <td><textarea type="text"></textarea></td>
-                                        <td><textarea type = "text"></textarea></td>
-                                        <td><textarea type = "text"></textarea></td>
-                                        <td><textarea type = "text"></textarea></td>
+                                        <td><textarea type="text"></textarea></td>
+                                        <td><textarea type="text"></textarea></td>
                                         <td><input type="text" readOnly value="Name"></input></td>
-                                        <td><input type = "date"></input></td>
-                                        
-                                    </tr>                    
-                                    </tbody>
+                                        <td><input type="date"></input></td>
+
+                                    </tr>
+
+
+                                </tbody>
                             </table>
-                        </div>
+                            <button type="button" class="btn btn-dark" onClick={this.adicionaNovaLinha}>Adicionar Linha</button>
+
+                        
                     </div>
                 </div>
                 <hr />

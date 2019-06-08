8ae82bb4b24bdcad7b2d29170f30f2b4b17598ef
Ricardo Valente Pinheiro <aluno20182@ipt.pt>
2019-06-08 11:14:35 +0100

    Criação da ficha de Teste de Solubilização

---
+++ Client/src/Components/TestesSolubilizacao/Create.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+/**
+ * Componente de criação de uma nova folha de obra
+ */
+
+class Create extends Component {
+
+    render() {
+        return (
+            <div className="container mb-3">
+                {/* Título */}
+                <div className="pt-3 py-3 text-center">
+                    <h2>Teste de Solubilização</h2>
+                </div>
+                
+            </div>
+        );
+    }
+
+}
+export default Create;

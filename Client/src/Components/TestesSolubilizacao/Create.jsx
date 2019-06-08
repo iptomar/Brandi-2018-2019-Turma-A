1aed98373a7ab3370a6e6d35fb5d62e798157f9e
Ricardo Valente Pinheiro <aluno20182@ipt.pt>
2019-06-08 18:49:01 +0100

    Adiciona e Elimina linha TesteSolubilização

--- Client/src/Components/TestesSolubilizacao/Create.jsx
+++ Client/src/Components/TestesSolubilizacao/Create.jsx
@@ -19,6 +19,42 @@ class Create extends Component {
         };
     }
 
+    eliminaUltimaLinha = () => {
+        var table = document.getElementById("table");
+        var numRows = table.rows.length;
+        if (numRows > 2) {
+            table.deleteRow(-1);
+        }
+    }
+
+    adicionaNovaLinha = () => {
+        var table = document.getElementById('table');
+        var row = table.insertRow(-1);
+        var novaCelula = row.insertCell(0);
+        // Coluna 1
+        var param = document.createElement('input');
+        novaCelula = row.insertCell(0);
+        param = document.createElement('textarea');
+        param.className = "form-control input";
+        param.style.resize = "none";
+        param.setAttribute("rows", "1");
+        novaCelula.appendChild(param);
+        // Coluna 2
+        param = document.createElement('input');
+        novaCelula = row.insertCell(1);
+        param.setAttribute('type', 'radio');
+        param.className = "EficaciaOptions";
+        novaCelula.appendChild(param);
+        // Coluna 3
+        novaCelula = row.insertCell(2);
+        param = document.createElement('textarea');
+        param.className = "form-control input";
+        param.style.resize = "none";
+        param.setAttribute("rows", "1");
+        novaCelula.appendChild(param);
+
+    }
+
     /**
     * Formulário de criação do teste de solubilização
     */
@@ -32,73 +68,75 @@ class Create extends Component {
                 <hr />
 
                 <label>Identificação do Estrato/Sujidade: </label>
-                <input type="text" className="form-control mb-3" id="localizacao" placeholder="Localização da obra" required />
+                <input type="text" className="form-control mb-3" id="idEstratoSujidade" placeholder="Localização da obra" required />
                 <hr />
 
                 <label>Características: </label>
-                <input type="text" className="form-control mb-3" id="localizacao" placeholder="Localização da obra" required />
+                <input type="text" className="form-control mb-3" id="caracteristicas" placeholder="Localização da obra" required />
                 <hr />
 
                 {/* Tabela */}
-                <table>
+                <table className="table table-bordered table-secondary text-center" id="table">
                     <thead>
                         <tr>
                             <th className="align-middle">Solvente ou Mistura de Solventes</th>
-                            <th className="align-middle">Grau de Eficácia Na Solubilização
-                            {/* RADIO BUTTONS */}
-                                <div className="input-group mb-3 col-md-2">
-                                    <div className="input-group-prepend">
-                                        <div className="input-group-text">
-                                            <input type="radio" name="EficaciaOptions" id="um" value="1" />
-                                        </div>
-                                    </div>
-                                    <label className="form-control" htmlFor="EficaciaUm">1</label>
-                                </div>
-
-                                <div className="input-group mb-3 col-md-2">
-                                    <div className="input-group-prepend">
-                                        <div className="input-group-text">
-                                            <input type="radio" name="EficaciaOptions" id="dois" value="2" />
-                                        </div>
-                                    </div>
-                                    <label className="form-control" htmlFor="EficaciaDois">2</label>
-                                </div>
-
-                                <div className="input-group mb-3 col-md-2">
-                                    <div className="input-group-prepend">
-                                        <div className="input-group-text">
-                                            <input type="radio" name="EficaciaOptions" id="tres" value="3" />
-                                        </div>
-                                    </div>
-                                    <label className="form-control" htmlFor="EficaciaTres">3</label>
-                                </div>
-
-                                <div className="input-group mb-3 col-md-2">
-                                    <div className="input-group-prepend">
-                                        <div className="input-group-text">
-                                            <input type="radio" name="EficaciaOptions" id="quatro" value="4" />
-                                        </div>
-                                    </div>
-                                    <label className="form-control" htmlFor="EficaciaQuatro">4</label>
-                                </div>
-
-                                <div className="input-group mb-3 col-md-2">
-                                    <div className="input-group-prepend">
-                                        <div className="input-group-text">
-                                            <input type="radio" name="EficaciaOptions" id="cinco" value="5" />
-                                        </div>
-                                    </div>
-                                    <label className="form-control" htmlFor="EficaciaCinco">5</label>
-                                </div>
-                            </th>
+                            <th className="align-middle">Grau de Eficácia Na Solubilização                            </th>
                             <th className="align-middle">Observações</th>
                         </tr>
                     </thead>
                     <tbody>
                         <tr>
-                            <td><textarea className="form-control input" style={{ resize: "none" }} rows="2" placeholder="HH:MM"/></td>
-                            <td><textarea className="form-control input" style={{ resize: "none" }} rows="2" placeholder="Técnico"/></td>
-                            <td><textarea className="form-control input" style={{ resize: "none" }} rows="2" placeholder="Observações"/></td>
+                            <td><textarea className="form-control input" style={{ resize: "none" }} rows="1" /></td>
+                            <td>                            
+                                <div className="row text-center">
+                                    {/* RADIO BUTTONS */}
+                                    <div className="input-group mb-3 col-md-2">
+                                        <div className="input-group-prepend">
+                                            <div className="input-group-text">
+                                                <input type="radio" name="EficaciaOptions" id="um" value="1" />
+                                            </div>
+                                        </div>
+                                        <label className="form-control" htmlFor="EficaciaUm">1</label>
+                                    </div>
+
+                                    <div className="input-group mb-3 col-md-2">
+                                        <div className="input-group-prepend">
+                                            <div className="input-group-text">
+                                                <input type="radio" name="EficaciaOptions" id="dois" value="2" />
+                                            </div>
+                                        </div>
+                                        <label className="form-control" htmlFor="EficaciaDois">2</label>
+                                    </div>
+
+                                    <div className="input-group mb-3 col-md-2">
+                                        <div className="input-group-prepend">
+                                            <div className="input-group-text">
+                                                <input type="radio" name="EficaciaOptions" id="tres" value="3" />
+                                            </div>
+                                        </div>
+                                        <label className="form-control" htmlFor="EficaciaTres">3</label>
+                                    </div>
+
+                                    <div className="input-group mb-3 col-md-2">
+                                        <div className="input-group-prepend">
+                                            <div className="input-group-text">
+                                                <input type="radio" name="EficaciaOptions" id="quatro" value="4" />
+                                            </div>
+                                        </div>
+                                        <label className="form-control" htmlFor="EficaciaQuatro">4</label>
+                                    </div>
+
+                                    <div className="input-group mb-3 col-md-2">
+                                        <div className="input-group-prepend">
+                                            <div className="input-group-text">
+                                                <input type="radio" name="EficaciaOptions" id="cinco" value="5" />
+                                            </div>
+                                        </div>
+                                        <label className="form-control" htmlFor="EficaciaCinco">5</label>
+                                    </div>
+                                </div>
+                            </td>
+                            <td><textarea className="form-control input" style={{ resize: "none" }} rows="1" /></td>
                         </tr>
                     </tbody>
 
@@ -112,9 +150,6 @@ class Create extends Component {
                             <button type="button" className="btn m-1 btn-secondary" onClick={this.eliminaUltimaLinha}>Remover linha</button>
                             <AlertMsg text={this.state.alertText} isNotVisible={this.state.alertisNotVisible} alertColor={this.state.alertColor} />
                         </div>
-                    </div>
-                    <div className="col-md-12 mt-2">
-                        <button type="button" className="btn btn-success btn-lg btn-block mb-5" onClick={this.submit}>Criar</button>
                     </div>
                 </div>              
             </div>

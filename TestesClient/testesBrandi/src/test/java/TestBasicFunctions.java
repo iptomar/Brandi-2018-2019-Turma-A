1b618173b8756b7e85e8ad5736c51b3f740f7d0f
Pedro Cruz <pm_cruz95@hotmail.com>
2019-06-10 23:48:33 +0100

    Remaining Tests Created

---
+++ TestesClient/testesBrandi/src/test/java/TestBasicFunctions.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+/**
+ *
+ * @author pm_cr
+ */
+public class TestBasicFunctions {
+    
+    static WebDriver driver = new ChromeDriver();
+    //variavel utilizada para fazer o logout no final do teste
+    boolean dologout = true;
+    
+    public TestBasicFunctions() {
+    
+       
+
+    }
+    
+    @BeforeClass
+    public static void setUpClass() throws InterruptedException {
+      
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+        driver.quit();
+    }
+    
+    @Before
+    public void setUp() {
+        
+    }
+    
+    @After
+    public void tearDown() {
+        //caso a variavel dologout estetja a true, é efetuado o logout selecionando 
+        //esta opçao na pagina "IndexFichaRIPage"
+        if (dologout) {
+        WebElement sair =  driver.findElement(By.className("nav-sair"));
+        sair.click();
+        } 
+    }
+    
+    
+    @Test
+    public void TestBasicFunctions() throws Exception {
+        // Create a new instance of the Firefox driver
+        // Notice that the remainder of the code relies on the interface, 
+        // not the implementation.
+        WebDriver driver = new FirefoxDriver();
+
+        // And now use this to visit NetBeans
+        driver.get("http://www.netbeans.org");
+        // Alternatively the same thing can be done like this
+        // driver.navigate().to("http://www.netbeans.org");
+
+        // Check the title of the page
+        // Wait for the page to load, timeout after 10 seconds
+        (new WebDriverWait(driver, 10)).until(new ExpectedCondition<Boolean>() {
+            @Override
+            public Boolean apply(WebDriver d) {
+                return d.getTitle().contains("NetBeans");
+            }
+        });
+
+        //Close the browser
+        driver.quit();
+    }
+    
+}
